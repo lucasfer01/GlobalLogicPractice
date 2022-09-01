922c0903bbf1be8b2aa6c4baac556df0333487da
Lucas Fernandez <lucas.fernandez@globallogic.com>
2022-09-01 20:43:15 -0300

    Ejercicio 2.1

---
+++ ejercicio2.1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SwitchColor from './components/SwitchColor/SwitchColor';
+
+function App() {
+  return (
+    <div className="App">
+      <SwitchColor/>
+    </div>
+  );
+}
+
+export default App;
