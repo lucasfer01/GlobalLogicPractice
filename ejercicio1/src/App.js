84443e16a3b4f5def445e5c2178607cb066767cd
Lucas Fernandez <lucas.fernandez@globallogic.com>
2022-08-31 21:00:53 -0300

    First two homeworks

---
+++ ejercicio1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul className='ul'>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
