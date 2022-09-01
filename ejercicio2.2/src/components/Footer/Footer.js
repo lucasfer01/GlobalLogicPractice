9122318186be193aa6c12ca0952dc5889aaf7a94
Lucas Fernandez <lucas.fernandez@globallogic.com>
2022-09-01 20:26:37 -0300

    Footer functional component

---
+++ ejercicio2.2/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer className='container-footer'>
+        <img src='/images/globallogic-logo.svg' alt='globallogic logo'/>
+
+        <span>2022 Copyright GlobalLogic Inc. All rights reserved.</span>
+    </footer>
+  )
+}
